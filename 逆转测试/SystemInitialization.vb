5cc3ede59a726858a4f864c41551f62ce3c1873b
st10-c17209 <Administrator@JUSTPLAY-PC>
2018-05-11 13:16:20 +0800

    修改开机启动数据库方式

--- 逆转测试/SystemInitialization.vb
+++ 逆转测试/SystemInitialization.vb
@@ -1,5 +1,6 @@
 ﻿Public Class SystemInitialization
-
+    '数据库链接
+    Public DatabaseCon As New SqlClient.SqlConnection("server=JUSTPLAY-PC\SQL;DataBase=datasave1;integrated security=true")
     Private Sub SystemInitialization_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         Timer1.Enabled = True
         ProgressBar1.Value = 30
@@ -10,11 +11,14 @@
 InitialLable:
         Try
             Application.DoEvents()
-            Call OpenConn() '刷新型号及产品参数数据库
+            DatabaseCon.Open()
+            '刷新型号及产品参数数据库
         Catch ex As Exception
             GoTo InitialLable
         End Try
         ProgressBar1.Value = 100
+        DatabaseCon.Close()
+
         Me.Close()
     End Sub
 End Class
